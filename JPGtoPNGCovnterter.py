5fe383ef94255f734fe323aa3aefd576ab22e6d4
unknown <farahmakram@gmail.com>
2020-09-08 12:27:56 +0100

    adding py project

---
+++ JPGtoPNGCovnterter.py
@@ -0,0 +1,14 @@
+import sys
+import os
+from PIL import Image
+
+# grab first and second arguments
+current_folder = sys.argv[1]
+new_folder = sys.argv[2]
+
+# check if new/ exists, if not, create it
+
+
+# loop through pokedex,
+# convert to png
+# save to the new folder
